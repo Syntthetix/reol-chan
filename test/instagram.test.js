c6641fdac6de742dfe88a8f5f91178de61a464db
yaojie <limyaojie93@gmail.com>
2018-10-29 11:57:23 +0800

    add stubbed tests for instagram and twitter functionality

---
+++ test/instagram.test.js
@@ -0,0 +1,3 @@
+describe.skip("Instagram", function() {
+  test("it should write to db and upload to file storage if it's new", () => {});
+});
--- test/twitter.test.js
+++ test/twitter.test.js
@@ -9,9 +9,22 @@ describe("Twitter", function() {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
     }
   };
-  test("it should scrape a post properly", () => {
+  test('it should scrape a post properly', () => {
     return twitter.getTwitterList(options).then(data => {
       expect(data).toBeDefined();
+      data.each((_, datum) => {
+        expect(typeof datum.text).toBe('string');
+        if (datum.image) {
+          expect(datum.images).toContain(datum.image);
+        } else {
+          expect(datum.images.length).toBe(0);
+        }
+        expect(datum.url).toBeTruthy();
+      });
     });
   });
+
+  test.skip('it should write to db and upload to file storage if it does not exist', () => {});
+  test.skip('it should not write to the database if it exists', () => {});
+  test.skip('it should save videos properly', () => {});
 });
